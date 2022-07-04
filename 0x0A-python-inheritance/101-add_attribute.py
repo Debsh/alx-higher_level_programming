def4549266a0cb01153a16f9a10cacf58b446871
Debora Taye <dibotaye3@gmail.com>
2022-07-04 12:08:50 -0700

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -16,22 +16,4 @@ def add_attribute(obj, a, v):
         """
     else:
         setattr(obj, a, v)
-        """ setattr adds new attribute """#!/usr/bin/python3
-"""
-File: 101-add_attribute.py - function that adds new attribute
-to an object if it’s possible:
-"""
-
-
-def add_attribute(obj, a, v):
-    """
-    add_attribute function is defined below
-    """
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("can't add new attribute")
-        """
-        If this has attribute, raise an error
-        """
-    else:
-        setattr(obj, a, v)
         """ setattr adds new attribute """
